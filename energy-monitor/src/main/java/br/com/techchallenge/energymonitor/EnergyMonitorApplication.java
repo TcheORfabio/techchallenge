984db9b5f849250b7174ecff5cf0fe2a80f7d41e
Fábio Ap. Oliveira Silva <fabio.oliveira.silva@avanade.com>
2023-05-01 19:54:44 -0300

    Primeiro Commit

---
+++ energy-monitor/src/main/java/br/com/techchallenge/energymonitor/EnergyMonitorApplication.java
@@ -0,0 +1,13 @@
+package br.com.techchallenge.energymonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergyMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnergyMonitorApplication.class, args);
+	}
+
+}
---
+++ energy-monitor/src/test/java/br/com/techchallenge/energymonitor/EnergyMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.techchallenge.energymonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnergyMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
